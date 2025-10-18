f47fc033d29d802316afa0d0730850e84fd428ea
hussenk <hussen199710@yahoo.com>
2025-10-18 14:04:15 +0200

    wip

--- app/Http/Controllers/FacebookWebhook.php
+++ app/Http/Controllers/FacebookWebhook.php
@@ -14,13 +14,32 @@ class FacebookWebhook extends Controller
     public function __invoke(Request $request)
     {
 
+
+        // Handle verification requests (GET) from Facebook
+        if ($request->isMethod('get')) {
+            // Facebook may send hub.verify_token or hub_verify_token depending on source
+            $verifyToken = $request->input('hub.verify_token') ?? $request->input('hub_verify_token');
+            $challenge = $request->input('hub.challenge') ?? $request->input('hub_challenge');
+
+            // Get expected token from SystemConfig or .env fallback
+            $expected = SystemConfig::getValue('facebook_verify_token') ?? getenv('FACEBOOK_VERIFY_TOKEN');
+
+            if ($verifyToken && $expected && hash_equals((string) $expected, (string) $verifyToken)) {
+                return response($challenge, 200);
+            }
+
+            return response('Forbidden', 403);
+        }
+
+        // For non-verification requests, persist the webhook payload
         Webhook::create([
-            'request_payload' => $request->all() ?? null,
+            'request_payload' => $request->all(),
             'method' => $request->method(),
             'ip_address' => $request->ip(),
             'status' => 'received',
         ]);
 
+        // Ensure any other startup side-effects can still use SystemConfig
         SystemConfig::getValue('facebook_app_id');
 
     }
--- database/seeders/SystemConfigSeeder.php
+++ database/seeders/SystemConfigSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\SystemConfig;
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,38 +15,45 @@ class SystemConfigSeeder extends Seeder
      */
     public function run(): void
     {
-        SystemConfig::create([
-            'key' => 'facebook_app_id',
-            'value' => '1472185290505642',
-        ]);
+        SystemConfig::createOrUpdate([
+            'key' => 'facebook_app_id', ],
+            [
+                'value' => '1472185290505642',
+            ]);
 
-        SystemConfig::create([
+        SystemConfig::createOrUpdate([
             'key' => 'facebook_token',
+        ], [
             'value' => 'EAAU68dlIvaoBPieIAkwxAaXTjZC572IwP16wxsvQv6HdmZBLdOmAK8wpfB0bZAGcJsHYer8rrgWCinqkP34rLCwh5NByZAekslqDKMHYhGR7h4Wouqoj0dPoeZCKZAtZCIisKTRXdLp0UTbO9cGMSM94bc1OXdsCluP3nqAKZCO3TR1xJoYHBdaHlwo20LOecEb70KSZCcZA9TfNByfDywEGFQxqN9bgq2laUzEGydbZAePXq9K6wZDZD',
         ]);
 
-        SystemConfig::create([
+        SystemConfig::createOrUpdate([
             'key' => 'facebook_secret',
+        ], [
             'value' => 'ca2830f93429ddef49eb8c5c0ff02cd7',
         ]);
 
-        SystemConfig::create([
+        SystemConfig::createOrUpdate([
             'key' => 'whatsapp_business_number_id',
+        ], [
             'value' => 'your_whatsapp_business_number_id_here',
         ]);
 
-        SystemConfig::create([
+        SystemConfig::createOrUpdate([
             'key' => 'whatsapp_business_phone_number',
+        ], [
             'value' => 'your_whatsapp_business_phone_number_here',
         ]);
 
-        SystemConfig::create([
+        SystemConfig::createOrUpdate([
             'key' => 'whatsapp_token',
+        ], [
             'value' => 'EAAU68dlIvaoBPieIAkwxAaXTjZC572IwP16wxsvQv6HdmZBLdOmAK8wpfB0bZAGcJsHYer8rrgWCinqkP34rLCwh5NByZAekslqDKMHYhGR7h4Wouqoj0dPoeZCKZAtZCIisKTRXdLp0UTbO9cGMSM94bc1OXdsCluP3nqAKZCO3TR1xJoYHBdaHlwo20LOecEb70KSZCcZA9TfNByfDywEGFQxqN9bgq2laUzEGydbZAePXq9K6wZDZD',
         ]);
 
-        SystemConfig::create([
+        SystemConfig::createOrUpdate([
             'key' => 'whatsapp_secret',
+        ], [
             'value' => 'your_whatsapp_secret_here',
         ]);
     }
